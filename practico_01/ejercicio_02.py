9acec89439b39f537cfaef935cea40b5cb44c864
Lautaro <lautarocoria5@gmail.com>
2021-03-30 18:08:17 -0300

    Auto stash before merge of "practico_01" and "origin/practico_01"

--- practico_01/ejercicio_02.py
+++ practico_01/ejercicio_02.py
@@ -15,21 +15,24 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
         return c
     
 
+    if b < a > c:
+        return a
+    elif a < b > c:
+        return b
+    elif a < c > b:
+        return c
+
+
 # NO MODIFICAR - INICIO
 assert maximo_encadenado(1, 10, 5) == 10
-assert maximo_encadenado(4, 9, 18) == 18
-assert maximo_encadenado(24, 9, 18) == 24
-# NO MODIFICAR - FIN
-
 
 ###############################################################################
 
 
 def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
-    """Re-escribir para que tome 4 parámetros, utilizar la función max.
-
-    Referencia: https://docs.python.org/3/library/functions.html#max"""
+    return max(a,b,c,d)
     pass # Completar
+
 
 
 # NO MODIFICAR - INICIO
