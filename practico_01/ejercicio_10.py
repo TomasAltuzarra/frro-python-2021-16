b226d6ce2aed6df002146eb7acaa8b007b8d1b5d
Tomas Altuzarra <e.tomasaltuzarra@gmail.com>
2021-04-06 21:02:02 -0300

    Resolucion Ejercicio 10

--- practico_01/ejercicio_10.py
+++ practico_01/ejercicio_10.py
@@ -6,7 +6,14 @@ from typing import Iterable
 def tiene_pares_basico(numeros: Iterable[int]) -> bool:
     """Toma una lista y devuelve un booleano en función si tiene al menos un
     número par."""
-    pass # Completar
+
+    result = False
+    for i in numeros:
+        if i%2==0:
+            result = True
+        else:
+            result = False
+    return result
 
 
 # NO MODIFICAR - INICIO
@@ -23,7 +30,15 @@ def tiene_pares_for_else(numeros: Iterable[int]) -> bool:
     """Re-Escribir utilizando for-else con dos return y un break.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
     """
-    pass # Completar
+ 
+    result = False
+    for i in numeros:
+        if i%2==0:
+            result = True
+            break
+    else:
+        return False
+    return result
 
 
 # NO MODIFICAR - INICIO
@@ -40,8 +55,8 @@ def tiene_pares_any(numeros: Iterable[int]) -> bool:
     """Re-Escribir utilizando la función any, sin utilizar bucles.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
-    pass # Completar
 
+    return any(num % 2 == 0 for num in numeros)
 
 # NO MODIFICAR - INICIO
 assert tiene_pares_any([1, 3, 5]) is False
